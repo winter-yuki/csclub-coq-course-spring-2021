bdda775d9d4a3fe6f6a509580ab439c163d54e35
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2021-03-21 00:51:36 +0300

    Add seminar

--- seminars/seminar01.v
+++ seminars/seminar01.v
@@ -4,24 +4,33 @@ From mathcomp Require Import ssreflect.
 
 Definition prodA (A B C : Type) :
   (A * B) * C -> A * (B * C)
-:=
+:= fun '(a, b, c) => (a, (b, c)).
 
 Definition sumA (A B C : Type) :
   (A + B) + C -> A + (B + C)
-:=
+:= fun s =>
+     match s with
+     | inr c => inr (inr c)
+     | inl (inl a) => inl a
+     | inl (inr b) => inr (inl b)
+     end.
 
 Definition prod_sumD (A B C : Type) :
   A * (B + C) -> (A * B) + (A * C)
-:=
+:= fun x =>
+     match x with
+     | (a, inl b) => inl (a, b)
+     | (a, inr c) => inr (a, c)
+     end.
 
-Definition sum_prodD (A B C : Type) :
+(* Definition sum_prodD (A B C : Type) :
   A + (B * C) -> (A + B) * (A + C)
-:=
+:=  *)
 
 
 (* function composition *)
 Definition comp A B C (f : B -> A) (g : C -> B) : C -> A
-:=
+:= fun x => f (g x).
 
 
 (* Introduce a notation that lets us use composition like so: f \o g.
@@ -29,17 +38,19 @@ Definition comp A B C (f : B -> A) (g : C -> B) : C -> A
 
 
 (* Introduce empty type, call `void` *)
+Inductive void : Type := .
 
 Definition void_terminal (A : Type) :
   void -> A
-:=
+:= fun v => match v with end.
 
 
 (* Introduce `unit` type (a type with exactly one canonical form) *)
+Inductive unit : Type := tt.
 
 Definition unit_initial (A : Type) :
   A -> unit
-:=
+:= fun _ => tt.
 
 
 (* Think of some more type signatures involving void, unit, sum, prod *)
